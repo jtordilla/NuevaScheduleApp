99290df66be217fb14c4eed00dd7403f675b0ac0
Jonathon T <jtordilla03@gmail.com>
2020-05-27 22:54:14 -0700

    Created basic app to test JSON

---
+++ index.js
@@ -0,0 +1,8 @@
+var loadSurvey = document.getElementById("load-select"); 
+var submitCourse = document.getElementById("submit-course"); 
+
+window.onload = function(){
+    loadSurvey.style.display = "none"; 
+    submitCourse.style.display = "none"; 
+}
+
