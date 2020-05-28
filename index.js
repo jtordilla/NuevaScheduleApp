f5146123a8c5cdceaf0595c0472cb2001a67adc5
Jonathon T <jtordilla03@gmail.com>
2020-05-27 23:15:43 -0700

    Created demo application with basic styling + code

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 var loadSurvey = document.getElementById("load-select"); 
 var submitCourse = document.getElementById("submit-course"); 
+var loadDesc = document.getElementById("load-desc"); 
+var courseInfo = document.getElementById("course-info"); 
 
 window.onload = function(){
     loadSurvey.style.display = "none"; 
     submitCourse.style.display = "none"; 
+}
+
+/*when user clicks button, new request is created to fetch data
+from JSON file and create renderable data using an external function*/
+loadDesc.addEventListener("click", function(){
+    var request = new XMLHttpRequest; 
+    request.open("GET", "https://raw.githubusercontent.com/jtordilla/VanillaJavascript/master/JSON_files/courses.json"); 
+    request.onload = function(){
+        var data = JSON.parse(request.responseText); 
+        renderHTML(data); 
+    }
+    request.send(); 
+}); 
+//render a string that takes the first item from the JSON file 
+function renderHTML(textObject){
+    htmlText = textObject[0].name; 
+    courseInfo.insertAdjacentHTML("beforeend", htmlText); 
 }
 
