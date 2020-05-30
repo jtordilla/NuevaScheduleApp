0c83f06fb6425ed02a84dd8d02a61b0a86ad9a2e
Jonathon T <jtordilla03@gmail.com>
2020-05-30 00:03:51 -0700

    finished prototype

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ var loadSurvey = document.getElementById("load-select");
 var submitCourse = document.getElementById("submit-course"); 
 var loadDesc = document.getElementById("load-desc"); 
 var engInfo = document.getElementById("eng-info"); 
-var counter = 0; 
 var fname = document.getElementById("fname"); 
 var lname = document.getElementById("lname"); 
 var email = document.getElementById("email"); 
-var submitBtn = document.querySelector("#submit-info"); 
+var submitBtn = document.querySelector("#submit-info");
+var readMore = document.getElementById("readMore"); 
+var listingHead = document.querySelector("#listingHead"); 
+
+window.onload = function(){
+    loadSurvey.style.display = "none"; 
+    submitCourse.style.display = "none"; 
+    readMore.style.display = "none"; 
+    listingHead.style.display = "none"; 
+}
 
 submitBtn.addEventListener("click", function(){
     if(fname.value == "" || fname.value === "First Name"){
@@ -18,33 +26,62 @@ submitBtn.addEventListener("click", function(){
     }
     else{
         console.log("successfully filled out"); 
+        var itemArray = ["fname", "lname", "email"]; 
+        for(var i = 0; i < itemArray.length; i++){
+            if(i == 0){
+                storeInfo("fname"); 
+                console.log(localStorage.getItem("fname")); 
+            }else if(i == 1){
+                storeInfo("lname"); 
+                console.log(localStorage.getItem("lname"));
+            }else if(i == 2){
+                storeInfo("email"); 
+                console.log(localStorage.getItem("email"));
+            }
+        }
     }
 }); 
 
-window.onload = function(){
-    loadSurvey.style.display = "none"; 
-    submitCourse.style.display = "none"; 
+function storeInfo(id){
+    var container = document.getElementById(id); 
+    if(id == "fname"){
+        localStorage.setItem("fname", container.value);
+    }else if(id == "lname"){
+        localStorage.setItem("lname", container.value); 
+    }else if(id == "email"){
+        localStorage.setItem("email", container.value);
+    }
 }
 
 /*when user clicks button, new request is created to fetch data
 from JSON file and create renderable data using an external function*/
 loadDesc.addEventListener("click", function(){
-    if(counter == 0){
-        var request = new XMLHttpRequest; 
-        request.open("GET", "https://raw.githubusercontent.com/jtordilla/VanillaJavascript/master/JSON_files/courses.json"); 
-        request.onload = function(){
-            var data = JSON.parse(request.responseText); 
-            renderHTML(data); 
-        }
-        request.send(); 
-        counter++; 
+    var request = new XMLHttpRequest; 
+    request.open("GET", "https://raw.githubusercontent.com/jtordilla/VanillaJavascript/master/JSON_files/courses.json"); 
+    request.onload = function(){
+        var data = JSON.parse(request.responseText); 
+        renderHTML(data); 
     }
+    request.send(); 
+    loadDesc.style.display = "none"; 
+    readMore.style.display = "block"; 
+    listingHead.style.display = "block"; 
 }); 
+
 //render a string that takes the first item from the JSON file 
 function renderHTML(textObject){
-    htmlText = "Mandatory: " + textObject[0].name + "<br>"; 
-    engInfo.insertAdjacentHTML("beforeend", htmlText); 
+    for(var i = 0; i < textObject.length; i++){
+        var htmlText = textObject[i].name + "<br>"; 
+        engInfo.insertAdjacentHTML("beforeend", htmlText); 
+    }
 }
+
+readMore.addEventListener("click", function(){
+    document.querySelector(".container").classList.toggle("expand"); 
+})
+
+
+
 
 
 
